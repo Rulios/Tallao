6d1187c800d94b36bf79de71beaef774f86c7af2
Robert Lu Zhen <robert_lu20@hotmail.com>
2021-02-07 00:33:19 -0500

    Try #2 to fix heroku deployment issue

--- controllers/Register/laundry.js
+++ controllers/Register/laundry.js
@@ -1,10 +1,27 @@
 const client = require("../libs/DB_CONNECT");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
-const {checkIfEmpty, trimAllExceptPassword} = require("../libs/inputs");
+/* const {checkIfEmpty, trimAllExceptPassword} = require("../libs/inputs"); */
 const LaundryOnce = require("./_laundry-once");
 const GenerateUniqueUUID = require("../libs/generate/unique-uuid");
 const saltRounds = 10;
+
+function checkIfEmpty(obj){
+    //iterates for all the keys to check if the value is empty
+    //If it's empty, it should return a false statement with a field value
+    //ONLY FOR STRING VALUES
+    Object.keys(obj).map(input =>{
+        if(validator.isEmpty(obj[input]))
+            return {exists: false, field: input};
+    })
+    return {exists: true};
+}
+
+function trimAllExceptPassword(inputs){
+    Object.keys(inputs).map(input =>{
+        if(input !== "password") inputs[input] = inputs[input].trim();
+    });
+}
 
 module.exports = function(register){
 
