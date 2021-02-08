95240daf26a6bf6f44637e777ebadef2f3496e35
Robert Lu Zhen <robert_lu20@hotmail.com>
2021-02-07 19:16:43 -0500

    Fixed feedback.ejs
    Added translation for Server-side-rendering. Made changes to ENV vars

--- controllers/Feedback/Controllers.js
+++ controllers/Feedback/Controllers.js
@@ -1,9 +1,11 @@
 const feedback = require("express").Router();
+const getLanguageStrings = require("../../translation/backend/get-language-strings");
+
 
 require("./submit")(feedback);
 
 feedback.get("/", function(req,res){
-    res.render("pages/feedback/feedback");
+    res.render("pages/feedback/feedback", Object.assign(getLanguageStrings(req)));
 });
 
 
--- controllers/libs/DB_CONNECT.js
+++ controllers/libs/DB_CONNECT.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 const {Client} = require('pg');
 
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_URL,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASS,
+    port: process.env.DATABASE_PORT,
 });
 
 client.connect();
